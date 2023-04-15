31d2aba6f124b292c6a2d564da2a71ea098799f3
Golden-Trio-12 <85909049+Golden-Trio-12@users.noreply.github.com>
2023-04-15 12:05:45 -0700

    Add files via upload

---
+++ book.js
@@ -0,0 +1,43 @@
+img = ""
+status = "";
+objects = [];
+
+function preload(){
+    img = loadImage('book.jpg')
+}
+
+function setup(){
+    canvas = createCanvas(420, 640)
+    canvas.center()
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"
+}
+
+function draw(){
+    image(img, 0, 0, 420, 640)
+    if(status !=""){
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence*100);
+            text(objects [i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x -15, objects[i].y -15, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("Model Loaded")
+    status = true;
+    objectDetector.detect(img,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
